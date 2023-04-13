0dfd8a994413b9902d5525763f9f63234a40b2d4
MamedKhuduzada <memmedxudiyev@gmail.com>
2023-04-13 04:43:31 -0700

    Add project files.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,67 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MyList<string> list = new MyList<string>();
+        list.Add("a");
+        list.Add("b");
+        list.Add("c");
+        list.Add("x");
+        list.Add("z");
+        list.Add("s");
+        Console.WriteLine(list.IndexOf("b"));
+    }
+}
+class MyList <T>
+    where T : class
+{
+    private T[] arr=new T[0];
+    private int Count { get; set; }
+    private int Capacity;
+
+    public void Add(T item)
+    {
+        if (Count == 0)
+        {
+            arr = new T[4];
+            arr[Count++] = item;
+        }
+        else if(Count==arr.Length)
+        {
+            Array.Resize(ref arr, arr.Length * 2);
+            arr[Count++] = item;   
+        }
+        else
+        {
+            arr[Count++] = item;
+        }
+    }
+
+    public int IndexOf(T item)
+    {
+        for (int i = 0; i < Count ; i++)
+        {
+            if (arr[i] == item)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    public void RemoveAt(T item)
+    {
+        int index = IndexOf(item);
+        T[] newArray = new T[arr.Length];
+        int j = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (index != i)
+            {
+                newArray[j++] = arr[i];
+            }
+        }
+        arr = newArray;
+    }
+}
